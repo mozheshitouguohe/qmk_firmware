60bb70e3df4602fcde99424529827fc8a31e1db7
Tobias Linder <tobiaslinder@Tobiass-MBP.lan>
2020-08-21 15:40:31 +0200

    fixed td_quotes

--- keyboards/ergodash/rev1/keymaps/mozheshitouguohe/keymap.c
+++ keyboards/ergodash/rev1/keymaps/mozheshitouguohe/keymap.c
@@ -50,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 				KC_ESC, 			KC_1, 			KC_2, 			KC_3, 			KC_4, 		KC_5, 		LSFT(KC_7), 															RALT(KC_MINS), 	KC_6, 		KC_7, 			KC_8, 		KC_9, 		KC_0, 		SE_PLUS, 
 				KC_TAB, 			KC_Q, 			KC_W, 			KC_E, 			KC_R, 		KC_T, 		KC_NUBS, 																LSFT(KC_NUBS), 	KC_Y, 		KC_U, 			KC_I, 		KC_O, 		KC_P, 		KC_LBRC, 
 				KC_CAPS, 			KC_A, 			KC_S, 			KC_D, 			KC_F, 		KC_G, 		LSFT(KC_8), 															LSFT(KC_9), 	KC_H, 		KC_J, 			KC_K, 		KC_L, 		KC_SCLN, 	KC_QUOT, 
-				TD(QUOTES), 	KC_Z, 			KC_X, 			KC_C, 			KC_V, 		KC_B, 																								KC_N, 		KC_M, 			KC_COMM,	KC_DOT, 	KC_UP, 		RSFT_T(KC_NUHS), 
+				TD(TD_QUOTES), 		KC_Z, 			KC_X, 			KC_C, 			KC_V, 		KC_B, 																								KC_N, 		KC_M, 			KC_COMM,	KC_DOT, 	KC_UP, 		RSFT_T(KC_NUHS), 
 				KC_LCTL, 			KC_LGUI, 		KC_LALT, 		KC_RALT, 									KC_BSPC, KC_SPC, LT(1,KC_ENT), 		LT(2,KC_ENT), KC_DEL, TD(TD_DOT_QMARK), 									KC_SLSH, 	KC_LEFT,	KC_DOWN, 	KC_RGHT),
 
 	[1] = LAYOUT_3key_2us(
