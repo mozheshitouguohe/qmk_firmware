b43d2eb63a7b51eff74aaa1b94297803baa82d5f
Tobias Linder <tobiaslinder@Tobiass-MBP.lan>
2020-11-01 17:43:52 +0100

    changes to layer 2

--- keyboards/ergodash/rev1/keymaps/mozheshitouguohe/keymap.c
+++ keyboards/ergodash/rev1/keymaps/mozheshitouguohe/keymap.c
@@ -196,7 +196,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                 `--------------------'       `--------------------'
  */
 	[0] = LAYOUT_3key_2us(
-				KC_ESC, 			KC_1, 			KC_2, 			KC_3, 			KC_4, 		KC_5, 		LSFT(KC_7), 															RALT(KC_MINS), 	KC_6, 		KC_7, 			KC_8, 		KC_9, 		            KC_0, 		SE_PLUS, 
+				KC_ESC, 			KC_1, 			KC_2, 			KC_3, 			KC_4, 		KC_5, 		LSFT(KC_7), 															TD(TD_BSLSH_PIPE), KC_6, 	KC_7, 			KC_8, 		KC_9, 		            KC_0, 		SE_PLUS, 
 				KC_TAB, 			KC_Q, 			KC_W, 			KC_E, 			KC_R, 		KC_T, 		TD(TD_LBRC_LCBR), 														TD(TD_RBRC_RCBR), KC_Y, 	KC_U, 			KC_I, 		KC_O, 		            KC_P, 		KC_LBRC, 
 				TD(TD_QUOTES), 		KC_A, 			KC_S, 			KC_D, 			KC_F, 		KC_G, 		TD(TD_LPRN_LT), 														TD(TD_RPRN_GT), KC_H, 		KC_J, 			KC_K, 		KC_L, 		            KC_SCLN, 	KC_QUOT, 
 				KC_LSFT, 			KC_Z, 			KC_X, 			KC_C, 			KC_V, 		KC_B, 																								KC_N, 		KC_M, 			KC_COMM,	KC_DOT, 	            KC_UP, 		RSFT_T(KC_EQL), 
@@ -210,9 +210,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 				KC_TRNS, 			KC_TRNS, 		KC_TRNS, 		KC_TRNS, 										KC_TRNS, KC_TRNS, KC_TRNS, 				KC_TRNS, KC_TRNS, KC_TRNS, 								        	KC_TRNS, 	TD(TD_HOME_SHIFT_HOME), KC_PGDN, 	TD(TD_END_SHIFT_END)),
 	
 	[2] = LAYOUT_3key_2us(
-				KC_NO, 				KC_F1, 			KC_F2, 			KC_F3, 			KC_F4, 		KC_F5, 		KC_NO, 																	KC_NO, 			KC_F6, 		KC_F7, 			KC_F8, 		KC_F9, 		            KC_F10, 	KC_F12, 
-				KC_NO, 				KC_AT, 			KC_NO, 			LSFT(KC_0), 	KC_NO, 		KC_NO, 		KC_NO, 																	KC_NO, 			KC_NO, 		LSFT(KC_SLSH), 	KC_NO, 		KC_NO, 		            LSFT(KC_5), KC_NO, 				
-				KC_NO, 				SE_AT,		 	LSFT(KC_NUHS), 	KC_NO, 			KC_NO, 		KC_NO, 		KC_NO, 																	KC_NO, 			LSFT(KC_3), KC_NO, 			KC_NO, 		KC_NO, 		            KC_NO, 		KC_NO, 				
+				KC_NO, 				KC_F1, 			KC_F2, 			KC_F3, 			KC_F4, 		KC_F5, 		SE_QUOT, 																SE_PIPE, 		KC_F6, 		KC_F7, 			KC_F8, 		KC_F9, 		            KC_F10, 	KC_F12, 
+				KC_NO, 				KC_AT, 			KC_NO, 			LSFT(KC_0), 	KC_NO, 		KC_NO, 		SE_LCBR, 																SE_RCBR, 		KC_NO, 		LSFT(KC_SLSH), 	KC_NO, 		KC_NO, 		            LSFT(KC_5), KC_NO, 				
+				KC_NO, 				SE_AT,		 	LSFT(KC_NUHS), 	KC_NO, 			KC_NO, 		KC_NO, 		SE_LABK, 																S(SE_LABK), 	LSFT(KC_3), KC_NO, 			KC_NO, 		KC_NO, 		            KC_NO, 		KC_NO, 				
 				KC_NO, 				KC_NO, 			KC_NO, 			KC_NO, 			KC_NO, 		KC_NO, 																								KC_NO, 		KC_SLSH, 		KC_NO, 		KC_NO, 		            KC_PGUP, 	KC_NO, 			
 				KC_NO, 				KC_NO, 			KC_NO, 			KC_NO, 										KC_NO, KC_SPC, KC_ENT, 				KC_ENT, KC_DEL, KC_TRNS, 													KC_NO, 		KC_LEFT, 	            KC_PGDN, 	KC_RGHT)
 };
